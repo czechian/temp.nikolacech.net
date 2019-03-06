8f2590b7c12d03890269158d3c008325ef7f585f
Nikola Čech <nikola.cech@gooddata.com>
2019-03-06 15:26:36 +0100

    JS height

--- keyboard/js/index.js
+++ keyboard/js/index.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  var container = $('.container')
-  
   $('#layout-option').on('change', function() {
     var value = this.value;
-    container
+    $('.container')
       .toggleClass('layout-flex', value === 'layout-flex')
       .toggleClass('layout-fixed', value === 'layout-fixed')
       .toggleClass('layout-sticky', value === 'layout-sticky')
       .toggleClass('layout-absolute', value === 'layout-absolute')
       .toggleClass('layout-mixed', value === 'layout-mixed');
+  });
+  
+  $(window).on('resize', function() {
+    document.getElementById('input').value = window.innerHeight;
   })
 });
